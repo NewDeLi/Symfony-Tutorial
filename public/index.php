cc313dbc8a26f4bb9156401c085604975d3bbf49
denis.lindner <denis.lindner@redhotmagma.de>
2023-01-30 10:08:11 +0100

    push local project

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
+
+
+//this script is always executed first and its job is to boot up symfony and run the app
